811a06b527878dbd4616618a3ff5dee76d9d0b79
Alex Di Guida <alexandre.diguida@gmail.com>
2022-03-13 09:29:39 +0000

    Code cleaning

--- experience/index.js
+++ experience/index.js
@@ -1,10 +1,7 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 import fragment from './shader/fragment.glsl'
 import vertex from './shader/vertex.glsl'
-
-import gsap from 'gsap'
 
 export default class Sketch {
   constructor(options) {
@@ -17,11 +14,6 @@ export default class Sketch {
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(this.width, this.height)
-    this.renderer.setClearColor(0xeeeeee, 1)
-    this.renderer.outputEncoding = THREE.sRGBEncoding
-
-    this.raycaster = new THREE.Raycaster()
-    this.pointer = new THREE.Vector2(-1, -1)
 
     this.textureLoader = new THREE.TextureLoader()
 
@@ -35,73 +27,50 @@ export default class Sketch {
     )
     this.camera.position.set(0, 0, 2)
 
-    // var frustumSize = 10;
-    // var aspect = window.innerWidth / window.innerHeight;
-    // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-
     this.time = 0
-
     this.isPlaying = true
-    this.isShowing = false
-    this.resetRotation = new THREE.Vector3()
-    this.currentSelection = null
-
-    this.addObjects()
-    this.resize()
-    this.render()
-    this.setupResize()
 
     this.materials = []
     this.meshes = []
     this.groups = []
 
-    this.handleImages()
-
-    this.onPointerMove = this.onPointerMove.bind(this)
-
-    window.addEventListener('pointermove', this.onPointerMove)
+    this.addObjects()
+    this.resize()
+    this.render()
+    this.setupResize()
+    this.setImages()
   }
 
-  onPointerMove(event) {
-    // calculate pointer position in normalized device coordinates
-    // (-1 to +1) for both components
-
-    this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1
-    this.pointer.y = -(event.clientY / window.innerHeight) * 2 + 1
-  }
-
-  handleImages() {
+  setImages() {
     let images = [...document.querySelectorAll('img')]
 
     images.forEach((img, i) => {
-      // const aspect = img.naturalWidth / img.naturalHeight or 1.4
+      // const imgAspect = img.naturalWidth / img.naturalHeight or 1.4
+      const group = new THREE.Group()
 
-      let mat = this.material.clone()
+      const geo = new THREE.PlaneBufferGeometry(1.4, 1, 20, 20)
+      const mat = this.material.clone()
       this.materials.push(mat)
-      let group = new THREE.Group()
-
       mat.uniforms.texture1.value = this.textureLoader.load(img.src)
       mat.uniforms.texture1.value.needsUpdate = true
 
-      let geo = new THREE.PlaneBufferGeometry(1.4, 1, 20, 20)
-      let mesh = new THREE.Mesh(geo, mat)
-      group.add(mesh)
-      this.groups.push(group)
-      this.scene.add(group)
-      this.meshes.push(mesh)
+      const mesh = new THREE.Mesh(geo, mat)
       mesh.position.y = i * 1.2
       mesh.name = `photo-${i}`
       mesh.userData = {
         index: i,
-        project: `My Project ${i}`,
-        github: 'https://github.com/alex-dg',
+        project: `Photo ${i}`,
+        link: 'https://github.com/alex-dg',
       }
 
+      group.add(mesh)
       group.rotation.x = -0.3
       group.rotation.y = -0.5
       group.rotation.z = -0.1
+
+      this.groups.push(group)
+      this.scene.add(group)
+      this.meshes.push(mesh)
     })
   }
 
@@ -110,6 +79,7 @@ export default class Sketch {
   }
 
   resize() {
+    // TODO: resize shader
     this.width = this.container.offsetWidth
     this.height = this.container.offsetHeight
     this.renderer.setSize(this.width, this.height)
@@ -133,7 +103,6 @@ export default class Sketch {
           value: new THREE.Vector2(1, 1),
         },
       },
-      // wireframe: true,
       transparent: true,
       vertexShader: vertex,
       fragmentShader: fragment,
@@ -151,117 +120,12 @@ export default class Sketch {
     }
   }
 
-  showDomDetails(data) {
-    console.log({ data })
-    this.projectName.innerText = data.project
-    this.projectUrl.innerText = data.github
-
-    const tl = gsap.timeline()
-    tl.to(this.project, {
-      opacity: 1,
-      display: 'flex',
-      duration: 1,
-      ease: 'power3.out',
-    })
-    tl.to(this.projectName, {
-      opacity: 1,
-      duration: 0.5,
-      ease: 'power3.out',
-    })
-    tl.to(this.projectUrl, {
-      opacity: 1,
-      duration: 0.5,
-      ease: 'power3.out',
-    })
-  }
-
-  showPhoto(index) {
-    if (!this.isShowing) {
-      this.clearTweens()
-      this.isShowing = true
-      this.currentSelection = index
-      const mesh = this.meshes[index]
-      this.resetRotation.copy(mesh.rotation.clone())
-
-      const tl = gsap.timeline()
-      tl.to(mesh.rotation, {
-        x: 0.2,
-        y: 0.5,
-        z: 0,
-        duration: 0.35,
-        ease: 'power3.inOut',
-      })
-      tl.to(this.camera.position, {
-        z: 1.7,
-        duration: 0.35,
-        ease: 'power3.inOut',
-      })
-      gsap.to(this.materials[this.currentSelection].uniforms.bendFactor, {
-        value: 0.0,
-        duration: 0.5,
-        ease: 'power3.inOut',
-      })
-    }
-  }
-
-  resetPhoto() {
-    if (typeof this.currentSelection === 'number') {
-      this.clearTweens()
-      const mesh = this.meshes[this.currentSelection]
-      const tl = gsap.timeline()
-      tl.to(mesh.rotation, {
-        x: 0,
-        y: 0,
-        z: 0,
-        duration: 0.35,
-        ease: 'power3.inOut',
-      })
-      tl.to(this.camera.position, {
-        z: 2,
-        duration: 0.35,
-        ease: 'power3.inOut',
-      })
-      gsap.to(this.materials[this.currentSelection].uniforms.bendFactor, {
-        value: 0.025,
-        duration: 0.5,
-        ease: 'power3.inOut',
-      })
-
-      this.currentSelection = null
-    }
-  }
-
-  clearTweens() {
-    gsap.globalTimeline.clear()
-  }
-
   render() {
     if (!this.isPlaying) return
 
     this.time += 0.05
 
-    this.materials?.forEach((m, i) => {
-      m.uniforms.time.value = this.time
-    })
-
-    // update the picking ray with the camera and pointer position
-    this.raycaster.setFromCamera(this.pointer, this.camera)
-
-    // calculate objects intersecting the picking ray
-    // const intersects = this.raycaster.intersectObjects(this.scene.children)
-    // if (intersects.length > 0 && !this.isProjectOpen) {
-    //   const photo = intersects[0].object
-    //   this.isProjectOpen = true
-    //   // this.showDomDetails(photo.userData)
-    //   // this.showPhoto(photo.userData.index)
-    // } else {
-    //   this.isShowing = false
-    //   // this.resetPhoto()
-    // }
-
-    // console.log({ isShowing: this.isShowing })
-
-    // this.material.uniforms.time.value = this.time
+    this.materials?.forEach((m, _) => (m.uniforms.time.value = this.time))
 
     requestAnimationFrame(this.render.bind(this))
 
--- main.js
+++ main.js
@@ -1,44 +1,26 @@
-import Sketch from './experience'
 import './style.css'
+
+import Sketch from './experience'
 import gsap from 'gsap'
 import * as THREE from 'three'
+
+/**
+ * Init experience
+ */
+const sketch = new Sketch({
+  dom: document.getElementById('container'),
+})
+
+/**
+ * Utils
+ */
 
 const isMobile =
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
   )
-console.log({ isMobile })
-let sketch = new Sketch({
-  dom: document.getElementById('container'),
-})
 
-let attracktMode = false
-let attracktTo = 0
-let speed = 0
-let position = 0
-let rounded = 0
-let wrap = document.getElementById('wrap')
-let elems = [...document.querySelectorAll('.n')]
-let dots = [...document.querySelectorAll('li')]
-
-if (isMobile) {
-  let startY
-
-  window.addEventListener('touchmove', (evt) => {
-    const deltaY = evt.touches[0].clientY - startY
-    speed += deltaY * 0.00003
-  })
-  window.addEventListener('touchstart', (e) => {
-    startY = e.touches[0].clientY
-  })
-} else {
-  window.addEventListener('wheel', (event) => {
-    speed += event.deltaY * 0.0002
-  })
-}
-
-let objs = Array(5).fill({ dist: 0 })
-
+// Handle max/min scroll position
 const minMaxPosition = () => {
   let pos = position
   if (position >= 0 && position <= elems.length - 1) {
@@ -53,6 +35,80 @@ const minMaxPosition = () => {
   return pos
 }
 
+/**
+ * Base
+ */
+let attracktMode = false
+let attracktTo = 0
+let speed = 0
+let position = 0
+let rounded = 0
+
+let objs = Array(5).fill({ dist: 0 })
+let rots = sketch.groups.map((g) => g.rotation)
+
+/**
+ * Dom Elements
+ */
+const wrap = document.getElementById('wrap')
+const elems = [...document.querySelectorAll('.n')]
+const dots = [...document.querySelectorAll('li')]
+const navs = [...document.querySelectorAll('li')]
+const nav = document.querySelector('.nav')
+
+/**
+ * Events - desktop & mobile
+ */
+if (isMobile) {
+  let startY
+  window.addEventListener('touchmove', (evt) => {
+    const deltaY = evt.touches[0].clientY - startY
+    speed += deltaY * 0.00003
+  })
+  window.addEventListener('touchstart', (e) => {
+    startY = e.touches[0].clientY
+  })
+} else {
+  window.addEventListener('wheel', (event) => {
+    speed += event.deltaY * 0.0002
+  })
+}
+
+// Dots navigation - on mouse enter
+nav.addEventListener('mouseenter', () => {
+  attracktMode = true
+
+  gsap.to(rots, {
+    x: -0.5,
+    y: 0,
+    z: 0,
+    duration: 0.35,
+    ease: 'power3.inOut',
+  })
+})
+
+// Dots navigation - on mouse leave
+nav.addEventListener('mouseleave', () => {
+  attracktMode = false
+
+  gsap.to(rots, {
+    x: -0.3,
+    y: -0.5,
+    z: -0.1,
+    duration: 0.35,
+    ease: 'power3.inOut',
+  })
+})
+
+navs.forEach((el) => {
+  el.addEventListener('mouseover', (e) => {
+    attracktTo = Number(e.target.getAttribute('data-nav'))
+  })
+})
+
+/**
+ * Update loop
+ */
 const tick = () => {
   position = minMaxPosition()
   speed *= 0.8
@@ -78,8 +134,7 @@ const tick = () => {
 
   rounded = Math.round(position)
 
-  let diff = rounded - position
-
+  const diff = rounded - position
   if (attracktMode) {
     position += -(position - attracktTo) * 0.04
   } else {
@@ -92,37 +147,3 @@ const tick = () => {
 }
 
 tick()
-
-const navs = [...document.querySelectorAll('li')]
-const nav = document.querySelector('.nav')
-
-let rots = sketch.groups.map((g) => g.rotation)
-nav.addEventListener('mouseenter', () => {
-  attracktMode = true
-
-  gsap.to(rots, {
-    x: -0.5,
-    y: 0,
-    z: 0,
-    duration: 0.35,
-    ease: 'power3.inOut',
-  })
-})
-
-nav.addEventListener('mouseleave', () => {
-  attracktMode = false
-
-  gsap.to(rots, {
-    x: -0.3,
-    y: -0.5,
-    z: -0.1,
-    duration: 0.35,
-    ease: 'power3.inOut',
-  })
-})
-
-navs.forEach((el) => {
-  el.addEventListener('mouseover', (e) => {
-    attracktTo = Number(e.target.getAttribute('data-nav'))
-  })
-})
